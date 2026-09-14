97f557a15dab5c45c7b2baadd17bd13578370268
OTs-14-Groza <yexincheng209@gmail.com>
2026-09-14 22:12:37 +0800

    Update C1 Begining locally

---
+++ C1 Begining/C1.1 Simple C++ programme/Project1/Project1/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>//header
+using namespace std;//standard library
+int main()
+{
+	cout << "Hello world" << endl;
+	cout << "Enter two number : " << endl;
+		int v1 = 0, v2 = 0;
+	cin >> v1 >> v2;
+	cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;//endl is manipulator
+	cout << "The product of " << v1 << " and " << v2 << " is " << v1 * v2 << endl;
+
+	return 0;
+}
+/*
+// used to add comments
+*/
